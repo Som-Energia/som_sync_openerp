4ac9abe123055d0426408b1590ba350634a286cb
Oriol Piera <oriol.piera@somenergia.coop>
2026-01-09 12:51:05 +0100

    ✅ test odoo sync _build_update_vals and minor changes. ir.model.data demo semanthic ids and method siganture to make it easy tot est

--- som_sync_openerp/tests/tests_odoo_sync.py
+++ som_sync_openerp/tests/tests_odoo_sync.py
@@ -29,3 +29,69 @@ class TestOdooSync(testing.OOTestCaseWithCursor):
     def test_check_erp_record_exist__Exception(self):
         with self.assertRaises(ERPObjectNotExistsException):
             self.os_obj.check_erp_record_exist(self.cursor, self.uid, 'res.partner', 123456)
+
+    def test__build_update_vals__syncPartnerAlreadySyncred__ok(self):
+        sync_id = self.imd_obj.get_object_reference(
+            self.cursor, self.uid, 'som_sync_openerp', 'odoo_partner_already_syncred'
+        )[1]
+        context = {
+            'sync_state': 'synced',
+            'update_last_sync': True,
+        }
+
+        vals, update = self.os_obj._build_update_vals(
+            self.cursor, self.uid, sync_id, 1001, '2024-06-10 12:00:00', context
+        )
+
+        expected_vals = {
+            'odoo_id': 1001,
+            'odoo_last_sync_at': '2024-06-10 12:00:00',
+            'sync_state': 'synced'
+        }
+        self.assertEqual(vals, expected_vals)
+        self.assertEqual(update, False)
+
+    def test__build_update_vals__syncPartnerAlreadySyncred__withError(self):
+        sync_id = self.imd_obj.get_object_reference(
+            self.cursor, self.uid, 'som_sync_openerp', 'odoo_partner_already_syncred'
+        )[1]
+        context = {
+            'sync_state': 'error',
+            'odoo_last_update_result': '{"message": "err", "error_code": "INTERNAL_SERVER_ERROR"}',
+            'update_last_sync': True,
+        }
+
+        vals, update = self.os_obj._build_update_vals(
+            self.cursor, self.uid, sync_id, 1001, '2024-06-10 12:00:00', context
+        )
+
+        expected_vals = {
+            'odoo_id': 1001,
+            'odoo_last_sync_at': '2024-06-10 12:00:00',
+            'odoo_last_update_result': '{"message": "err", "error_code": "INTERNAL_SERVER_ERROR"}',
+            'sync_state': 'error'
+        }
+        self.assertEqual(vals, expected_vals)
+        self.assertEqual(update, True)
+
+    def test__build_update_vals__syncCountryStateError_withOk(self):
+        sync_id = self.imd_obj.get_object_reference(
+            self.cursor, self.uid, 'som_sync_openerp', 'odoo_country_state_error'
+        )[1]
+        context = {
+            'sync_state': 'synced',
+            'update_last_sync': True,
+        }
+
+        vals, update = self.os_obj._build_update_vals(
+            self.cursor, self.uid, sync_id, 1001, '2024-06-10 12:00:00', context
+        )
+
+        expected_vals = {
+            'odoo_id': 1001,
+            'odoo_last_sync_at': '2024-06-10 12:00:00',
+            'odoo_last_update_result': '',
+            'sync_state': 'synced'
+        }
+        self.assertEqual(vals, expected_vals)
+        self.assertEqual(update, False)
